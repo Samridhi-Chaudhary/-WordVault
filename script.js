e610b7fcdc4b4e77ca8b7024052734678fbc15cf
Samridhi Chaudhary <samridhi.chaudhary16@gmail.com>
2025-07-08 22:14:04 +0530

    Search Functionality completed

--- script.js
+++ script.js
@@ -1,55 +1,83 @@
-navigator.geolocation.getCurrentPosition(
-    (position)=>{
-    const latitude= position.coords.latitude;
-    const longitude= position.coords.longitude;
-    // console.log(`Latitude: ${latitude}, Longitude: ${longitude}` );
-    getWeather(latitude,longitude);
-},(error)=>{
-    console.log("Error getting location", error);
-})
+// require('dotenv').config();
 
-function getWeather(latitude, longitude){
-    const apiKey="48fa6c66222c23ed8e31e547fb247f2a";
-    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+navigator.geolocation.getCurrentPosition(
+    (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        // console.log(`Latitude: ${latitude}, Longitude: ${longitude}` );
+        getWeather(latitude, longitude);
+    }, (error) => {
+        console.log("Error getting location", error);
+    })
+
+function getWeather(latitude, longitude) {
+    const apiKey = "f9071d7b47fd8c35db7b6e98bd2e07be";
+    // const apiKey = process.env.apiKey;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
     fetch(url)
-    .then(response=>response.json())
-    .then(data=>{
-        const location=data.name;
-        const weatherDescription=data.weather[0].description;
-        const temprature= (data.main.temp-273.15).toFixed(2);
-        const iconCode=data.weather[0].icon;
-        const iconUrl=`http://openweathermap.org/img/wn/${iconCode}@2x.png`;
-        
-        document.getElementById("location").innerHTML=`${temprature}°C ${location}`;
-        document.getElementById("weather").innerHTML=`${weatherDescription}`;
-        document.getElementById("icon").innerHTML=`<img src="${iconUrl}" alt="Weather Icon" width="40">`;
-    })
-    .catch(error=>{
-        console.log("Error fetching weather data", error);
-    })
+        .then(response => response.json())
+        .then(data => {
+            const location = data.name;
+            const weatherDescription = data.weather[0].description;
+            const temprature = (data.main.temp - 273.15).toFixed(2);
+            const iconCode = data.weather[0].icon;
+            const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+            document.getElementById("location").innerHTML = `${temprature}°C ${location}`;
+            document.getElementById("weather").innerHTML = `${weatherDescription}`;
+            document.getElementById("icon").innerHTML = `<img src="${iconUrl}" alt="Weather Icon" width="40">`;
+        })
+        .catch(error => {
+            console.log("Error fetching weather data", error);
+        })
 }
 
 
-// function searchMeaning(){
-//     const word=document.getElementById(searchWord).value.trim();
+async function searchMeaning() {
+    const word = document.getElementById("searchWord").value.trim();
 
-//     if(!word){
-//         document.getElementById("meaning").textContent="Please enter a word";
-//         return;
-//     }
+    if (!word) {
+       alert("Please enter a word");
+       document.getElementById("meaning").textContent="";
+        document.getElementById("meaning").textContent="";
+        return;
+    }
 
-//     const url=`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    try{
+        const response= await fetch(url);
+        if(!response.ok){
+            throw new Error("Word not found");
+        }
 
-//     fetch(url)
-//         .then(response=>response.json())
-//         .then(data=>{
-//             const mean=data.meanings[1].definition;
-//             const example=data.meanings[1].example;
-//             console.log(mean);
-//             console.log(example);
-//         })
-//         .catch(error=>{
-//             console.log(`Error fetching meaning of ${word}`);
-//         })
-//     }
+        const data= await response.json();
+
+        const definition= data[0].meanings[0].definitions[0].definition;
+        const example=data[0].meanings[0].definitions[0].example;
+        // console.log(definition);
+        // console.log(example);
+        document.getElementById("meaning").textContent= definition;
+        document.getElementById("example").textContent= example||"No Example available";
+    }
+    catch(error){
+        
+        alert(`Error: ${error.message}`);
+
+    }
+
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         const meaning = data[0].meanings[0].definitions[0].definition;
+    //         // console.log(mean);
+    //                     const example=data[0].meanings[0].definitions[0].example;
+    //         // console.log(example ||"No example available.");
+    //         document.getElementById("meaning").innerHTML=`${meaning}`;
+    //         document.getElementById("example").innerHTML=`${example || "No example available"}  `;
+
+    //     })
+    //     .catch(error => {
+    //         console.log(`Error fetching meaning of ${word}`);
+    //     })
+}
